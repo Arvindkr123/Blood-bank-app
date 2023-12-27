7863b0a95d40da0d0687070b0c4150925b16d2f1
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-27 12:19:54 +0530

    added login checking successfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import { UserModel } from './../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+// register controller
 export const registerController = async (req, res) => {
   try {
     const existingUser = await UserModel.findOne({ email: req.body.email });
@@ -24,5 +27,47 @@ export const registerController = async (req, res) => {
     res
       .status(500)
       .send({ error: 'Error in Register Api', success: false, error });
+  }
+};
+
+// login controller
+export const loginController = async (req, res) => {
+  try {
+    const existingUser = await UserModel.findOne({ email: req.body.email });
+    // if existingUser is not found then do this
+    if (!existingUser) {
+      return res
+        .status(404)
+        .send({ success: false, message: 'User not found' });
+    }
+
+    // if user exists then comparing password
+    const comparePassword = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    );
+    if (!comparePassword) {
+      return res
+        .status(500)
+        .send({ success: false, message: 'Invalid credentials' });
+    }
+    const token = jwt.sign(
+      { userId: existingUser._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '20d',
+      }
+    );
+    return res.status(200).send({
+      success: true,
+      message: 'login successfully',
+      token,
+      existingUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ success: false, message: 'Error in Login Api', error });
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
+import {
+  registerController,
+  loginController,
+} from '../controllers/authController.js';
 const authRouter = express.Router();
 
+// register method POST
 authRouter.post('/register', registerController);
+
+// Login method POST
+authRouter.post('/login', loginController);
 
 export default authRouter;
